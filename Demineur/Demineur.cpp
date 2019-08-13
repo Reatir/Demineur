918cb59bf0660758eedecf674627db553808829d
Reatir <bastien.fernandes.3g3@gmail.com>
2019-08-13 17:00:51 +0200

    v1

--- Demineur/Demineur.cpp
+++ Demineur/Demineur.cpp
@@ -3,19 +3,31 @@
 
 #include "pch.h"
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+char introduction();
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	if (introduction() == 'y')
+	{
+		cout << "oui" << endl;
+	}
+	else
+	{
+		cout << "non" << endl;
+	}
+
 }
 
-// Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
-// Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
-
-// Conseils pour bien démarrer : 
-//   1. Utilisez la fenêtre Explorateur de solutions pour ajouter des fichiers et les gérer.
-//   2. Utilisez la fenêtre Team Explorer pour vous connecter au contrôle de code source.
-//   3. Utilisez la fenêtre Sortie pour voir la sortie de la génération et d'autres messages.
-//   4. Utilisez la fenêtre Liste d'erreurs pour voir les erreurs.
-//   5. Accédez à Projet > Ajouter un nouvel élément pour créer des fichiers de code, ou à Projet > Ajouter un élément existant pour ajouter des fichiers de code existants au projet.
-//   6. Pour rouvrir ce projet plus tard, accédez à Fichier > Ouvrir > Projet et sélectionnez le fichier .sln.
+char introduction()
+{
+	char t;
+	cout << "Bienvenue!" << endl;
+	cout << "Bande de casse-cous!" << endl;
+	cout << "Pret a trouver des mines ?(y/n)" << endl;
+	cin >> t;
+	return t;
+}
